a0ace77cea049835cfd84a6aee2e7c27d07b022c
Gerard Coll <gerard@skrit.es>
2023-11-08 09:42:13 +0100

    final commit

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,8 +1,10 @@
 'use server';
+import { signIn } from "@/auth";
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { z } from "zod";
+
 
 const InvoiceSchema = z.object({
     id: z.string(),
@@ -107,7 +109,7 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete Invoice');
+    // throw new Error('Failed to Delete Invoice');
    
     // Unreachable code block
     try {
@@ -117,4 +119,18 @@ export async function deleteInvoice(id: string) {
     } catch (error) {
       return { message: 'Database Error: Failed to Delete Invoice' };
     }
+}
+
+export async function authenticate(
+        prevState: string | undefined,
+        formData: FormData,
+    ) {
+        try {
+            await signIn('credentials', Object.fromEntries(formData));
+        } catch (error) {
+            if ((error as Error).message.includes('CredentialsSignin')) {
+                return 'CredentialSignin';
+            }
+            throw error;
+        }
 }
